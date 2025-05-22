0b91187fdd34b8437a2676babd6cb81467e28c9d
rchieee <nueno.291906@laspinas.sti.edu.ph>
2025-05-22 13:05:08 +0800

    Update archie.py

--- archie.py
+++ archie.py
@@ -607,36 +607,16 @@ class Interpreter(NodeVisitor):
 
 
 def main():
-    text = """
-PROGRAM Test;
-VAR
-   number     : INTEGER;
-   a, b, c, x : INTEGER;
-   y          : REAL;
-
-BEGIN {Part10}
-   BEGIN
-      number := 2;
-      a := number;
-      b := 10 * a + 10 * number DIV 4;
-      c := a - - b
-   END;
-   x := 11;
-   y := 20 / 7 + 3.14;
-   { writeln('a = ', a); }
-   { writeln('b = ', b); }
-   { writeln('c = ', c); }
-   { writeln('number = ', number); }
-   { writeln('x = ', x); }
-   { writeln('y = ', y); }
-END.  {Part10}
-    """
+    import sys
+    text = open(sys.argv[1], 'r').read()
 
     lexer = Lexer(text)
     parser = Parser(lexer)
     interpreter = Interpreter(parser)
     result = interpreter.interpret()
-    print(interpreter.GLOBAL_SCOPE)
+
+    for k, v in sorted(interpreter.GLOBAL_SCOPE.items()):
+        print('{} = {}'.format(k, v))
 
 
 
